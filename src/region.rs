dd5ce759d2a8d54383bdcc7df3caaea1383c4ba5
Jan Adä <jan.adae@outlook.de>
2020-10-22 23:22:41 +0200

    minor change

--- src/region.rs
+++ src/region.rs
@@ -1,11 +1,9 @@
 use serde::Deserialize;
 
 #[derive(Deserialize, Copy, Clone, Debug, PartialEq, Eq, Hash)]
+#[serde(rename_all = "snake_case")]
 pub enum Region {
-    #[serde(rename = "ncsa")]
     Ncsa,
-    #[serde(rename = "emea")]
     Emea,
-    #[serde(rename = "apac")]
     Apac,
 }
