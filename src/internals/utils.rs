2eaedf498097be27b6abd7c261a4da7cc900fdc7
Jan Adä <jan561.github@gmail.com>
2020-11-03 21:15:15 +0100

    simplified check username

--- src/internals/utils.rs
+++ src/internals/utils.rs
@@ -1,46 +1,24 @@
-use crate::{Error, Platform};
+use crate::Error;
 use serde::{Deserialize, Deserializer};
 
-pub fn check_username(username: &str, platform: Platform) -> Result<(), Error> {
-    if username.is_empty() {
-        return Err(Error::UsernameMalformed);
-    }
-
-    match platform {
-        Platform::Pc => check_username_pc(username),
-        Platform::Xbox => check_username_xbox(username),
-        Platform::Playstation => check_username_playstation(username),
-    }
-}
-
-fn check_username_pc(username: &str) -> Result<(), Error> {
-    const ALLOWED_SPECIAL_CHARS: &str = ".-_";
-
-    _check_username(username, ALLOWED_SPECIAL_CHARS)
-}
-
-fn check_username_xbox(username: &str) -> Result<(), Error> {
-    _check_username(username, " ")
-}
-
-fn check_username_playstation(username: &str) -> Result<(), Error> {
-    const ALLOWED_SPECIAL_CHARACTERS: &str = "-_";
-
-    _check_username(username, ALLOWED_SPECIAL_CHARACTERS)
-}
-
-fn _check_username(username: &str, special_chars: &str) -> Result<(), Error> {
-    if valid_username(username, special_chars) {
+pub fn check_username(username: &str) -> Result<(), Error> {
+    if valid_username(username) {
         Ok(())
     } else {
         Err(Error::UsernameMalformed)
     }
 }
 
-fn valid_username(username: &str, special_chars: &str) -> bool {
+fn valid_username(username: &str) -> bool {
+    const ALLOWED_SPECIAL_CHARS: &str = ".-_ ";
+
+    if username.is_empty() {
+        return false;
+    }
+
     username
         .chars()
-        .all(|c| c.is_alphanumeric() || special_chars.contains(c))
+        .all(|c| c.is_alphanumeric() || ALLOWED_SPECIAL_CHARS.contains(c))
 }
 
 pub fn serde_parse_f64_option<'de, D>(deserializer: D) -> Result<Option<f64>, D::Error>
@@ -62,38 +40,16 @@ fn parse_f64<E: serde::de::Error>(s: &str) -> Result<f64, E> {
 #[cfg(test)]
 mod tests {
     use super::{check_username, serde_parse_f64_option};
-    use crate::Platform;
 
     #[test]
     fn test_check_username() {
-        // PC
-        let valid = "pengu.g2";
-        let invalids = ["", "pengu.g2/pc/generic", "pengu g2"];
+        let valid = "Hello.Hello Hello-Hello_Hello";
+        let invalids = ["", "Hello/Hello", "Hello#Hello"];
 
-        assert!(check_username(valid, Platform::Pc).is_ok());
-
-        for &invalid in invalids.iter() {
-            assert!(check_username(invalid, Platform::Pc).is_err());
-        }
-
-        // Xbox
-        let valid = "Hello Hello";
-        let invalids = ["", "Hello.Hello", "Hello/Hello"];
-
-        assert!(check_username(valid, Platform::Xbox).is_ok());
+        assert!(check_username(valid).is_ok());
 
         for &invalid in invalids.iter() {
-            assert!(check_username(invalid, Platform::Xbox).is_err());
-        }
-
-        // Playstation
-        let valid = "Hello_Hello-Hello";
-        let invalids = ["", "Hello.Hello", "Hello Hello", "Hello/Hello"];
-
-        assert!(check_username(valid, Platform::Playstation).is_ok());
-
-        for &invalid in invalids.iter() {
-            assert!(check_username(invalid, Platform::Playstation).is_err());
+            assert!(check_username(invalid).is_err());
         }
     }
 
--- src/stats/http.rs
+++ src/stats/http.rs
@@ -39,7 +39,7 @@ impl RouteBuilder {
             .expect("Error creating route: Platform missing.");
         let kind = self.kind.expect("Error creating route: Kind missing.");
 
-        check_username(&username, platform)?;
+        check_username(&username)?;
 
         let route = RouteInfo {
             username,
